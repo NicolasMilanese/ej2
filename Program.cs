c0f0228efe739bdc2dc3b12624b91251beb25a63
NicolasMilanese <nicolasmilanese2004@gmail.com>
2022-10-13 21:15:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ejercicio
+{
+class Program
+{
+static void Main (string[] args)
+{
+Console.Clear();
+Console.WriteLine("Escriba su nombre completo");
+string nombrePersona = Console.ReadLine ();
+Console.WriteLine ("hola " + nombrePersona);
+}
+
+}
+}
